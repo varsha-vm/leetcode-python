0d86e06cb161dfc5d841234f63fc5dfab2deea4f
Varsha Vaman Murthy <varshavamanmurthy@Varshas-MacBook-Air.local>
2025-12-01 21:02:29 -0800

    Add solution LC solutions

---
+++ hashmaps/LC_217_contains_duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = set()
+        for i in nums:
+            if i in visited:
+                return True
+            
+            visited.add(i)
+        return False
+        
---
+++ sliding_window/LC_3_longest_substring.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l=r=0
+        visited = set()
+        max_len = 0
+
+        for r in range(len(s)):
+            while s[r] in visited:
+                visited.remove(s[l])
+                l+=1
+            
+            visited.add(s[r])
+            max_len = max(max_len, r-l+1)
+
+        return max_len
