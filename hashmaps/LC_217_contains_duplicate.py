e9271843ae12ed9652b911a90b25f0e35026310a
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2025-12-03 10:25:29 -0800

    Adding solution to LC 217

--- hashmaps/LC_217_contains_duplicate.py
+++ hashmaps/LC_217_contains_duplicate.py
@@ -1,3 +1,5 @@
+#Time:O(N)
+#Space:O(N)
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         visited = set()
@@ -8,3 +10,5 @@ class Solution:
             visited.add(i)
         return False
         
+
+
