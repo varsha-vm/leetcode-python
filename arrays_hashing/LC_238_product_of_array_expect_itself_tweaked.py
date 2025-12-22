cbea62b3deb2d8c7147aac129e5240c54a26137b
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2025-12-22 13:46:26 -0800

    adding DSA solution

---
+++ arrays_hashing/LC_238_product_of_array_expect_itself_tweaked.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        result = [1] * len(nums)
+        left = 1
+
+        for i in range(len(nums)):
+            result[i] = left
+            left *= nums[i]
+
+        right = 1
+        for i in range(len(nums)-1, -1, -1):
+            result[i] *=right
+            right *= nums[i]
+
+        return result
