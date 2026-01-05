e7204f696a9370846db480cb58f784290d65a205
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-01-05 13:03:31 -0800

    Adding solution to remove element problem

---
+++ array/Remove element.py
@@ -0,0 +1,13 @@
+#Time: O(N)
+#Space: O(1)
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l = 0
+
+        for r in range(len(nums)):
+            if nums[r]!=val:
+                nums[l]=nums[r]
+                l+=1
+
+        return l
+        
