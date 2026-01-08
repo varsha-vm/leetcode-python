812e04123f2387980bfe58b96272949c8df7a8f9
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-01-08 11:29:18 -0800

    Adding solution to valid palindrome problem

---
+++ array/Valid palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        l=0
+        r=len(s)-1
+
+        while l<=r:
+            
+            #Skip non alpha numeric and spaces from left
+            while l<r and not s[l].isalnum():
+                l+=1
+
+            #Skip non alpha numeric and spaces from right
+            while r>l and not s[r].isalnum():
+                r-=1
+
+            if s[l].lower()!=s[r].lower():
+                return False
+
+            l+=1
+            r-=1
+       
+        return True
