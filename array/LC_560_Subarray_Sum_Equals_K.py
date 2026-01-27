054cbf9f211298b417e11942a8af6246287718c0
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-01-27 06:50:15 -0800

    Adding solution to LC 560

---
+++ array/LC_560_Subarray_Sum_Equals_K.py
@@ -0,0 +1,19 @@
+#Approach use hashmap to store all cumulative seen + initialize hashmap with 0:1
+#Time:O(N)
+#Space:O(N)
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        hashmap = {0:1}
+        count = 0
+        cur_sum = 0
+
+        for num in nums:
+            cur_sum+=num
+
+            if cur_sum - k in hashmap:
+                count+=hashmap[cur_sum-k]
+
+            hashmap[cur_sum] = hashmap.get(cur_sum, 0)+1
+
+        return count
+        
