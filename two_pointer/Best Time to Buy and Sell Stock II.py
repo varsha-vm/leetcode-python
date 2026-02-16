c8e4e58b5b1369791b7fe682fe67da96361a569a
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-02-15 22:25:09 -0800

    adding solution to best time to sell stock II

---
+++ two_pointer/Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,12 @@
+#Approach : greedy
+#Time: O(N)
+#Space : O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        total_profit = 0
+    
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                total_profit+= (prices[i] - prices[i-1])
+
+        return total_profit
